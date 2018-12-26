791ad21165f080f66f910693f70d0bfa2f0d667f
deniz salman <ddenizsalman@gmail.com>
2018-12-26 23:38:08 +0300

    Add files via upload
    2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import firebase from 'firebase';
-import { Header } from './components/common';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import firebase from '@firebase/app';
+import ReduxThunk from 'redux-thunk';
+import reducers from './reducers';
 import LoginForm from './components/LoginForm';
+import Router from './Router';
 
 class App extends Component {
-  componentWillmount() {
-    firebase.initializeApp({
-    apiKey: 'AIzaSyCaKbauHyWlilnzvtj9xSOaFnM47kva2PE',
-    authDomain: 'auth-500a1.firebaseapp.com',
-    databaseURL: 'https://auth-500a1.firebaseio.com',
-    projectId: 'auth-500a1',
-    storageBucket: 'auth-500a1.appspot.com',
-    messagingSenderId: '9106014853'
-  });
-  }
+    componentWillMount() {
+        const config = {
+            apiKey: 'AIzaSyCaKbauHyWlilnzvtj9xSOaFnM47kva2PE',
+            authDomain: 'auth-500a1.firebaseapp.com',
+            databaseURL: 'https://auth-500a1.firebaseio.com',
+            projectId: 'auth-500a1',
+            storageBucket: 'auth-500a1.appspot.com',
+            messagingSenderId: '9106014853'
+          };
+          
+          firebase.initializeApp(config);
+    }
 
-  render() {
-    return (
-      <View>
-        <Header headerText={'Authentication'} />
-        <LoginForm />
-      </View>
-    );
+    render() {
+        const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+        return (
+        <Provider store={store}>
+            <Router />
+        </Provider>
+        );
   }
 }
 
